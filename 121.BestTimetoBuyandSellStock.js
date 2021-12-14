01710d424fe8cd6a8da036229333f31f43cc5e7f
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2021-12-14 18:34:04 -0500

    121 completed

--- 121.BestTimetoBuyandSellStock.js
+++ 121.BestTimetoBuyandSellStock.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
+    let buyPrice = prices[0]; //1
+    let profit = 0; //4
+
+        for(let i =0; i < prices.length - 1; i++){
+            let tempProfit = prices[i+1] - prices[i];
+            if(tempProfit > 0) {
+                if(prices[i] < buyPrice) {
+                    buyPrice = prices[i];
+                }
+                if(prices[i+1] - buyPrice > profit) {
+                    profit = prices[i+1] - buyPrice;
+                }
+            }
+        }
+    return profit;
 };
